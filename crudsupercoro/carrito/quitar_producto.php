c2e47fccb894cf98324e926d8b9319c817d32ee0
sebastian <araton51103@gmail.com>
2025-05-26 17:22:06 -0500

    Actualización del panel admin con nuevas modificaciones

--- crudsupercoro/carrito/quitar_producto.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$id = $_GET['id'];
-unset($_SESSION['carrito'][$id]);
-header("Location: carrito.php");
-exit();
--- crudsupercoro/usuarios/includes/admin_check.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['usuario']) || $_SESSION['usuario']['rol'] !== 'admin') {
-    header("Location: ../../usuarios/login.php");
-    exit();
-}
